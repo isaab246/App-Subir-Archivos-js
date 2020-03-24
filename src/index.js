17589c3e945606979e00e13a3e5ded3b6ec5787f
Isaac <isaab246@gmail.com>
2020-03-24 12:32:14 -0600

    ya me desespero el polyfill

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
+import "@babel/polyfill";
 
 // import { init } from './js/chistes-page';
 // import { init } from './js/usuarios-page';
